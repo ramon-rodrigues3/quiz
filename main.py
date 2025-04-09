4c998e6d50a53df1114f8a2020f047b47f333ca1
Ramon Rodrigues <ramon333rodr@gmail.com>
2025-04-09 20:23:45 -0300

    versão acessável para vercel

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-@app.get("/home")
-def home_page():
-    return {"mensagem": "Olá, Mundo!"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates("pages")
+
+@app.get("/")
+def home_page(request: Request):
+    return templates.TemplateResponse("index.html", {'request': request})
+
+@app.get("/licao", name='licao')
+def licao_page(request: Request, num: int):
+    return templates.TemplateResponse(f"licao_{num}.html", {'request': request})
+
+@app.get("/quiz", name="quiz")
+def quiz_page(request: Request):
+        return templates.TemplateResponse("quiz.html", {'request': request})
+
+@app.get("/sobre", name="sobre")
+def sobre_page(request: Request):
+     return templates.TemplateResponse("sobre.html", {'request': request})
